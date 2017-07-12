45f85775c8525eb6583b402f09cb15d10bb28509
trorvillelim <orville.lim@tooltwist.com>
2017-07-12 13:55:00 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+
+build_num = $CIRCLE_BUILD_NUM
+echo "var BUILD_VERSION_STAGE = {
+          major: 0,
+          minor: 0,
+          build: $build_num
+      };
+
+      var BUILD_VERSION_PROD = {
+          major: 0,
+          minor: 0,
+          build: $build_num
+      };" > Build.js
